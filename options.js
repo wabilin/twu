535e1f83855639cd144e4ec67c0a207d09bf2c73
Ting Shu Lin <2549826+wabilin@users.noreply.github.com>
2024-02-19 17:54:23 +0900

    Use ESLint (AirBnB Rule) (#1)

--- options.js
+++ options.js
@@ -1,20 +1,20 @@
 const DANS = [
   // 0    1      2     3     4     5     6      7    8      9
- 	"新人","９級","８級","７級","６級","５級","４級","３級","２級","１級",
+  '新人', '９級', '８級', '７級', '６級', '５級', '４級', '３級', '２級', '１級',
   // 10    11    12    13    14    15    16     17   18     19
-	"初段","二段","三段","四段","五段","六段","七段","八段","九段","十段",
+  '初段', '二段', '三段', '四段', '五段', '六段', '七段', '八段', '九段', '十段',
   // 20
-	"天鳳位"
+  '天鳳位',
 ];
 const DEFAULT_PALYERS = ['トトリ先生19歳'];
 
-let subscriptedNames = undefined;
-let subscriptedDan = undefined;
+let subscriptedNames;
+let subscriptedDan;
 
 function saveOptions() {
-  let namesJson = undefined
+  let namesJson;
   if (subscriptedNames !== undefined) {
-    namesJson = JSON.stringify(subscriptedNames)
+    namesJson = JSON.stringify(subscriptedNames);
   }
   browser.storage.sync.set({
     subscriptedDan,
@@ -23,21 +23,21 @@ function saveOptions() {
 }
 
 function addNameToTable(namesBody, name) {
-  const row = namesBody.insertRow(namesBody.rows.length)
+  const row = namesBody.insertRow(namesBody.rows.length);
 
   const sell0 = row.insertCell(0);
-  sell0.appendChild( document.createTextNode(name) )
+  sell0.appendChild(document.createTextNode(name));
   const sell1 = row.insertCell(1);
 
-  const x = document.createElement('button')
+  const x = document.createElement('button');
   x.appendChild(document.createTextNode('x'));
 
-  sell1.appendChild(x)
+  sell1.appendChild(x);
   x.onclick = () => {
-    namesBody.removeChild(row)
-    subscriptedNames = subscriptedNames.filter(val => val !== name)
-    saveOptions()
-  }
+    namesBody.removeChild(row);
+    subscriptedNames = subscriptedNames.filter(val => val !== name);
+    saveOptions();
+  };
 }
 
 function restoreNames(namesJson) {
@@ -45,68 +45,79 @@ function restoreNames(namesJson) {
     subscriptedNames = JSON.parse(namesJson);
   }
 
-  subscriptedNames = subscriptedNames || DEFAULT_PALYERS
+  subscriptedNames = subscriptedNames || DEFAULT_PALYERS;
 
-  const namesBody = document.querySelector('#subscripted-names-table-body')
-  namesBody.innerHTML = ''
+  const namesBody = document.querySelector('#subscripted-names-table-body');
+  namesBody.innerHTML = '';
 
-  subscriptedNames.forEach(name => {
-    addNameToTable(namesBody, name)
-  })
+  subscriptedNames.forEach((name) => {
+    addNameToTable(namesBody, name);
+  });
 
-  const newPlayerInput = document.getElementById("new-player-name-input")
-  const newPlayerBtn = document.getElementById("new-player-name-btn")
+  const newPlayerInput = document.getElementById('new-player-name-input');
+  const newPlayerBtn = document.getElementById('new-player-name-btn');
   const addNameFromInput = () => {
-    const name = newPlayerInput.value
-    if (!name || subscriptedNames.includes(name)) { return }
+    const name = newPlayerInput.value;
+    if (!name || subscriptedNames.includes(name)) { return; }
 
     newPlayerInput.value = '';
     subscriptedNames = [...subscriptedNames, name];
     addNameToTable(namesBody, name);
-    saveOptions()
-  }
+    saveOptions();
+  };
   newPlayerBtn.onclick = addNameFromInput;
 
   newPlayerInput.onkeypress = (e) => {
     if (e.keyCode === 13) {
-      addNameFromInput()
+      addNameFromInput();
       return false;
     }
     return true;
   };
 }
 
-function restoreDan(dan) {
-  const selector = document.getElementById('dan-select');
-  for (let i = selector.options.length - 1; i >= 0; i -= 1) {
-    selector.remove(i);
+function parseDan(dan) {
+  if (Number.isInteger(dan)) {
+    return dan;
+  } if (dan && typeof (dan) === 'string') {
+    return parseInt(dan, 10);
   }
+  return undefined;
+}
 
-  if (dan && typeof(dan) === 'string') {
-    dan = parseInt(dan, 10)
+function clearSelectOptions(select) {
+  for (let i = select.options.length - 1; i >= 0; i -= 1) {
+    select.remove(i);
   }
+}
+
+function restoreDan(danStored) {
+  const dan = parseDan(danStored);
+
+  const selector = document.getElementById('dan-select');
+  clearSelectOptions(selector);
 
   // 7 dan ~ tenhou
-  const options = ['x', 16, 17, 18, 19, 20].map(val => {
-    const option = document.createElement("option");
+  const options = ['x', 16, 17, 18, 19, 20].map((val) => {
+    const option = document.createElement('option');
     option.text = val === 'x' ? 'x' : DANS[val];
-    option.value = val
+    option.value = val;
     if (val === dan || (val === 'x' && !dan)) {
-      option.selected = true
+      option.selected = true;
     }
 
-    return option
-  })
+    return option;
+  });
 
-  options.forEach(option => selector.add(option))
+  options.forEach(option => selector.add(option));
 
   selector.onchange = () => {
-    const selected = selector.value
-    subscriptedDan = selected
-    saveOptions()
-    selector.blur()
-    return false
-  }
+    const selected = selector.value;
+    subscriptedDan = selected;
+    saveOptions();
+    selector.blur();
+    return false;
+  };
 }
 
 function restoreOptions() {
@@ -116,10 +127,10 @@ function restoreOptions() {
   // });
 
   browser.storage.sync.get(['subscriptedNames', 'subscriptedDan'])
-  .then(res => {
-    restoreNames(res.subscriptedNames)
-    restoreDan(res.subscriptedDan)
-  });
+    .then((res) => {
+      restoreNames(res.subscriptedNames);
+      restoreDan(res.subscriptedDan);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
